779fb5122e0a9597a22c394b9177acdccec8e771
Kiano <dinnymalakiano@gmail.com>
2025-03-12 12:50:03 +0700

    baru

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Seeders;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+
+class UserSeeder extends Seeder {
+    public function run(): void {
+        User::insert([
+            [
+                'name' => 'Kiano',
+                'email' => 'admin@gmail.com',
+                'password' => Hash::make('admin123'),
+                'role' => 'admin',
+                'created_at' => now(),
+            ],
+            [
+                'name' => 'Karyawan Borongan',
+                'email' => 'karyawanborongan@gmail.com',
+                'password' => Hash::make('karyawanborongan123'),
+                'role' => 'karyawan_borongan',
+                'created_at' => now(),
+            ],
+            [
+                'name' => 'Karyawan Bulanan',
+                'email' => 'karyawanbulanan@gmail.com',
+                'password' => Hash::make('karyawanbulanan123'),
+                'role' => 'karyawan_bulanan',
+                'created_at' => now(),
+            ]
+        ]);
+    }
+}
